f4515b796d61ea179d0e732bcef58c3ee86e56c3
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2020-06-22 18:50:13 +0200

    added cors middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from inference import handler
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class SentenceInference(BaseModel):
     sentence: str
